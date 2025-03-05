2ac9dcf73363a53c988624cf3d5943b7f8d51850
thiago <thiago.w3c@gmail.com>
2025-03-05 17:59:45 -0300

    review object dot and bracket notation

---
+++ object-dot-bracket-notation.js
@@ -0,0 +1,20 @@
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+}
+
+// Dot Notation
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interestedIn = "job";
+
+console.log(jonas.interestedIn);
+console.log(jonas[interestedIn]);
